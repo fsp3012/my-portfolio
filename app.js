e8c71212cc46005859e3b68da88b5585a6b2adcc
fsp3012 <fakoredeabdulafeezolamide@gmail.com>
2023-09-26 11:46:53 +0100

    portfolio pic

--- app.js
+++ app.js
@@ -48,41 +48,10 @@ inp3.addEventListener("focusin", (e) => {
   emailjs.init("ZVxC0fvY0UaJbpS3z");
 })();
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   console.log("submitted");
-//   console.log(emailRq);
-//   let allFieldsGood = false;
-//   if (nameInp === null || nameInp === "") {
-//     nameRq.style.display = "block";
-//     return;
-//   }
-//   if (emailInp === null || emailInp === "") {
-//     emailRq.style.display = "block";
-//     return;
-//   }
-//   if (msgInp === null || msgInp === "") {
-//     msgRq.style.display = "block";
-//     return;
-//   }
-//   allFieldsGood = true;
-
-//   if (allFieldsGood) {
-//     console.log("isgone");
-//   }
-// });
-
-// ================================
-
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-
-    // generate a five digit number for the contact_number variable
-    // this.contact_number.value = (Math.random() * 100000) | 0;
-    // these IDs from the previous steps
-
     let allFieldsGood = false;
     if (nameInp === null || nameInp === "") {
       nameRq.style.display = "block";
@@ -114,59 +83,3 @@ document
       );
     }
   });
-
-// =====================================
-
-// name1.addEventListener("change", (e) => {
-//   nameInp = e.target.value;
-//   console.log(nameInp);
-//   nameRq.style.display = "none";
-// });
-// email.addEventListener("change", (e) => {
-//   emailInp = e.target.value;
-//   console.log(emailInp);
-//   emailRq.style.display = "none";
-// });
-// msg.addEventListener("change", (e) => {
-//   msgInp = e.target.value;
-//   console.log(msgInp);
-//   msgRq.style.display = "none";
-// });
-// button.addEventListener("click", () => {
-//   if (nameInp == null) {
-//     nameRq.style.display = "block";
-//   }
-//   if (emailInp == null) {
-//     emailRq.style.display = "block";
-//   }
-//   if (msgInp == null) {
-//     msgRq.style.display = "block";
-//   } else {
-//     firstDiv.classList.remove("d-flex");
-//     firstDiv.classList.add("d-none");
-//     secondDiv.classList.add("d-flex");
-//     secondDiv.classList.remove("d-none");
-//   }
-// });
-
-// =========================================
-
-// window.onload = function () {
-//   document
-//     .getElementById("contact-form")
-//     .addEventListener("submit", function (event) {
-//       event.preventDefault();
-//       // generate a five digit number for the contact_number variable
-//       this.contact_number.value = (Math.random() * 100000) | 0;
-//       // these IDs from the previous steps
-//       emailjs.sendForm("contact_service", "contact_form", this).then(
-//         function () {
-//           console.log("SUCCESS!");
-
-//         },
-//         function (error) {
-//           console.log("FAILED...", error);
-//         }
-//       );
-//     });
-// };
